1b0c1e6371b44036396431da6cd2fb0a2fdbc22c
Kurt <kespindler@gmail.com>
2012-01-13 07:05:20 -0800

    Initial Commit

---
+++ Money Travels/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Money Travels
+//
+//  Created by Kurt Spindler on 1/13/12.
+//  Copyright (c) 2012 Brown University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Money Travels/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Money Travels
+//
+//  Created by Kurt Spindler on 1/13/12.
+//  Copyright (c) 2012 Brown University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
